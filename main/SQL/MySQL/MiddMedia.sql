220d5316c90c49ebd342d96832f8aa1c9d56befa
Adam Franco <afranco@middlebury.edu>
2011-05-12 11:51:31 -0400

    Added update script and SQL table change for the queue quality field.

--- main/SQL/MySQL/MiddMedia.sql
+++ main/SQL/MySQL/MiddMedia.sql
@@ -18,5 +18,6 @@ CREATE TABLE IF NOT EXISTS `middmedia_queue` (
   `upload_time` timestamp NOT NULL default CURRENT_TIMESTAMP,
   `processing` tinyint(1) NOT NULL default '0',
   `processing_start` timestamp NULL default NULL,
+  `quality` varchar(20) collate utf8_bin default NULL,
   PRIMARY KEY  (`directory`,`file`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='A queue for file uploads that need conversion.';
